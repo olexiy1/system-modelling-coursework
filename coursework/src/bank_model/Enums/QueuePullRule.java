0f3b2ba1a73ffe6b53164f1ffcbde1b50d5b5705
olexiy1 <65558208+olexiy1@users.noreply.github.com>
2026-03-17 00:20:30 +0200

    Delete coursework/src/bank_model directory

--- coursework/src/bank_model/Enums/QueuePullRule.java
+++
@@ -1,6 +0,0 @@
-package bank_model.Enums;
-
-public enum QueuePullRule {
-    FIFO,
-    LIFO
-}
--- coursework/src/bank_model/Enums/Role.java
+++
@@ -1,7 +0,0 @@
-package bank_model.Enums;
-
-public enum Role {
-    CarCasier,
-    BuildingCashier,
-    Manager
-}
